aea3ba9fa5f61a599efa38bfb61c3500fd5323f8
Lee Chastain <leechastain@gamestop.com>
2013-11-19 08:25:34 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lcware::Application.config.secret_token = 'fec2bcf556017c9abcada7e6d6d9a9feac8a2d7c5d1930c10e14c40c03c706f334bceb254803d8a546d68270ace316c9b9e242b4853358fc96cb4514e7ed8f0c'

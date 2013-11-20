43d9f32a7efaa117d04b415a7762961277d889be
Lee Chastain <leechastain@gamestop.com>
2013-11-20 06:43:51 -0600

    Added the projects resource, changed from sqlite to postgres, added a default route

--- Gemfile
+++ Gemfile
@@ -5,11 +5,6 @@ gem 'rails', '3.2.13'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-group :development do
-  gem 'sqlite3'
-end
-
-
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
@@ -39,8 +34,9 @@ gem 'jquery-rails'
 # To use debugger
 # gem 'debugger'
 
+gem 'pg', '0.15.1'
+
 group :production do
-  gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Lcware::Application.routes.draw do
+  resources :projects
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -47,7 +50,7 @@ Lcware::Application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
+  # just remember to delete public/index.orig.html.
   # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
@@ -55,4 +58,6 @@ Lcware::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  root to: 'projects#index'
 end

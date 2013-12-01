169ce44421392dff26e86d81d1be8de51ddefea9
Lee Chastain <leechastain@gamestop.com>
2013-12-01 16:27:41 -0600

    Adding assets to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Lcware::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js.erb, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( balloon.config.js balloon.js bootstrap.js box.js prototype.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
